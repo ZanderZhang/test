ae55872c74e56f0c3a0660ce2419cad4d2e32d65
Frank <965461820@qq.com>
2015-07-22 22:08:17 +0800

    initial commit2

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by ZY on 7/22/15.
+//  Copyright (c) 2015 pointStone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
